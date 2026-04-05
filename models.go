8cdf4563cdeb2090bda3042fe8eac1ec6f0c8497
Ramya-Samala <samalaramyausa@gmail.com>
2026-04-04 22:51:10 -0500

    Update models.go
    Added remaining fields to model

--- models.go
+++ models.go
@@ -3,4 +3,9 @@ package main
 type HealthCheck struct {
 	ID       string `json:"id"`
 	Endpoint string `json:"endpoint"`
+	Status   string `json:"status,omitempty"`
+	Code     *int32 `json:"code,omitempty"` // pointer so we can distinguish 0 from "not checked yet"
+	Checked  int64  `json:"checked,omitempty"`
+	Duration string `json:"duration,omitempty"`
+	Error    string `json:"error,omitempty"`
 }
