2e524bc12081202737e7a6b8ed2b071e99e72b7b
Ramya-Samala <samalaramyausa@gmail.com>
2026-04-05 20:01:37 -0500

    Update main.go
    added basic server startup with store

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+	"net/http"
+	"time"
+)
 
+func main() {
+	addr := flag.String("bind", "127.0.0.1:8080", "listen address")
+	freq := flag.String("checkfrequency", "30s", "background check interval")
+	flag.Parse()
+
+	interval, err := time.ParseDuration(*freq)
+	if err != nil {
+		log.Fatalf("invalid checkfrequency: %v", err)
+	}
+
+	db := NewStore()
+	if err := db.Load(); err != nil {
+		log.Fatalf("loading data: %v", err)
+	}
+
+	log.Printf("listening on %s", *addr)
+	err = http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
