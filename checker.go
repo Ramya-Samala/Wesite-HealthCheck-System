dc6199140379b52b745fcb413bca591ea92c003d
Ramya-Samala <samalaramyausa@gmail.com>
2026-04-05 19:33:06 -0500

    Update checker.go
    Updated checker.go with record duration and checked time

--- checker.go
+++ checker.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
 func RunCheck(hc *HealthCheck, timeout time.Duration) {
 	cl := &http.Client{Timeout: timeout}
+
+	t0 := time.Now()
 	resp, err := cl.Get(hc.Endpoint)
+	took := time.Since(t0)
+
+	hc.Checked = time.Now().Unix()
+	hc.Duration = fmt.Sprintf("%dms", took.Milliseconds())
+
 	if err != nil {
 		hc.Status = "Error"
+		hc.Code = 0
 		return
 	}
 	resp.Body.Close()
 	hc.Status = http.StatusText(resp.StatusCode)
+	hc.Code = int32(resp.StatusCode)
 }
